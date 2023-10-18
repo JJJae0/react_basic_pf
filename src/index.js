60d17363b7969b73c0b6d5667b51f94cbef6d26b
JJJae0 <jaeyeong345@naver.com>
2023-10-18 10:18:53 +0900

    메인 페이지 갤러리 출력

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import youtubeReducer from './redux/youtubeSlice';
+import flickrReducer from './redux/flickrSlice';
 
 // youtubeReducer 반환한 데이터를 youtube property 담아서 객체형태로 store 에 등록
 // store 에는 하나의 객체만 등록가능하기 때문에 여러개의 데이터 카테고리는 reducer 로 통합해서 등록
 const store = configureStore({
 	reducer: {
 		youtube: youtubeReducer,
+		flickr: flickrReducer,
 	},
 });
 
